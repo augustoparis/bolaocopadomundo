4eac85464fa563482db4f5db4ec2121e4be23b84
Eduardo Malherbi Martins <emalherbi@gmail.com>
2014-06-05 16:38:53 -0300

    alter menu

--- app/view/mn-menu.php
+++ app/view/mn-menu.php
@@ -20,20 +20,10 @@
 			<ul class="nav navbar-nav">
 				<li><hr/></li>
 				<li class="active"><a id="menu-inicio" href="javascript:void(0)" ><?= L::menu_option_1; ?></a></li>
-				<li><a id="menu-games" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_2; ?></a></li>
+				<li class="active"><a href="javascript:void(0)" ><?= L::menu_option_2; ?></a></li>
+				<li><a id="menu-users" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_3; ?></a></li>
+				<li><a id="menu-games" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_4; ?></a></li>
 				<li><hr/></li>
-				<li class="active"><a href="javascript:void(0)" >Cadastros</a></li>
-				<li><a id="menu-users" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span>Usuários</a></li>
-				<li class="active"><a id="menu-inicio" href="javascript:void(0)" ><?= L::menu_option_1; ?></a></li>
-				<li><a id="menu-operadores" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_2; ?></a></li>
-				<li><hr/></li>
-				<li class="active"><a href="javascript:void(0)" ><?= L::menu_option_3; ?></a></li>
-				<li><ul class="nav navbar-nav">
-						<li><a id="menu-servidor" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_4; ?></a></li>
-						<li><a id="menu-aplicativo" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_5; ?></a></li>
-				</ul></li>
-				<li><hr/></li>
-				<li><a id="menu-descriptografia" href="javascript:void(0)" ><span class="glyphicon glyphicon-chevron-right"></span><?= L::menu_option_6; ?></a></li>
 			</ul>
 		</div>
 	</div>
